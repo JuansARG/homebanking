ed506ba3173ed6ca746299b9c817c4f83085ffc9
JuanS <97115938+JuansARG@users.noreply.github.com>
2022-12-16 18:12:28 -0300

    Guardando cambios

--- bin/main/static/assets/scripts/accounts.js
+++ bin/main/static/assets/scripts/accounts.js
@@ -1,23 +1,27 @@
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
-    data(){
-        return{
-            cuentas : {},
+    data() {
+        return {
             cliente: {},
+            cuentas: {},
+            prestamos: {},
         }
     },
-    created(){
+    created() {
         this.cargarDatos();
     },
-    methods:{
-        cargarDatos(){
-            axios.get("http://localhost:8080/api/clients/1")
-            .then(respuesta => {
-                this.cliente = respuesta.data;
-                this.cuentas = respuesta.data.account;
-                          
-            })
+    methods: {
+        cargarDatos() {
+            this.id = new URLSearchParams(document.location.search).get("id");
+            axios.get("http://localhost:8080/api/clients/" + this.id)
+                .then(respuesta => {
+                    this.cliente = respuesta.data;
+                    this.cuentas = this.cliente.account;
+                    this.prestamos = this.cliente.loans.sort((a, b) => a.id - b.id);
+                    console.log(this.prestamos);
+                })
+                .catch(e => console.log(e));
         }
     }
 }).mount("#app");
--- bin/main/static/assets/scripts/manager.js
+++ bin/main/static/assets/scripts/manager.js
@@ -1,10 +1,10 @@
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
-    data(){
-        return{
-            datos: undefined,
-            clientes:undefined,
+    data() {
+        return {
+            data: "",
+            clientes: [],
             nombre: "",
             apellido: "",
             email: "",
@@ -14,91 +14,85 @@ createApp({
             uri: "",
         }
     },
-    created(){
+    created() {
         this.cargarDatos();
     },
-    methods:{
-
-        cargarDatos(){
-            axios.get("http://localhost:8080/rest/clients")
-            .then((respuesta)=>{
-                this.datos = respuesta.data;
-                this.clientes = respuesta.data._embedded.clients;
-            })
-            .catch(e => console.log(e));
+    methods: {
+        cargarDatos() {
+            axios.get("http://localhost:8080/api/clients")
+                .then((respuesta) => {
+                    this.data = respuesta.data;
+                    this.clientes = respuesta.data;
+                    this.uri = "http://localhost:8080/rest/clients/";
+                })
+                .catch(e => console.log(e));
         },
 
-        agregarCliente(){
-            if(this.nombre != "" && this.apellido != "" && this.email !="" && this.email.includes("@")){
+        agregarCliente() {
+            if (this.nombre != "" && this.apellido != "" && this.email != "" && this.email.includes("@")) {
                 this.enviarCliente(this.nombre, this.apellido, this.email);
                 Swal.fire({
                     icon: "success",
                     text: "Customer added",
-                    confirmButtonColor: "#F5AE53"
                 })
-            }else{
+            } else {
                 Swal.fire({
                     icon: "error",
                     text: "Invalid values",
-                    confirmButtonColor: "#F5AE53"
                 });
             }
 
-            
             this.nombre = "";
             this.apellido = "";
             this.email = "";
         },
 
-        enviarCliente(nombre, apellido, email){
+        enviarCliente(nombre, apellido, email) {
             axios.post("http://localhost:8080/rest/clients", {
                 firstName: nombre,
                 lastName: apellido,
                 email: email
             })
-            .then(this.cargarDatos)
-            .catch(e => console.log(e));
+                .then(this.cargarDatos)
+                .catch(e => console.log(e));
         },
 
-        borrarCliente(cliente){
+        borrarCliente(cliente) {
             Swal.fire({
                 title: 'Are you sure?',
                 text: "You won't be able to revert this!",
                 icon: 'warning',
                 showCancelButton: true,
-                confirmButtonColor: '#F5AE53',
-                cancelButtonColor: '#31087B',
                 confirmButtonText: 'Yes, delete it!'
-              }).then((result) => {
+            }).then((result) => {
                 if (result.isConfirmed) {
-                    Swal.fire({
-                        icon: "success",
-                        text: "Deleted customer",
-                        confirmButtonColor: "#F5AE53"
-                    });
+                    this.uri = this.uri + cliente.id;
+                    axios.delete(this.uri)
+                        .then(result => {
+                            this.cargarDatos();
+                            Swal.fire({
+                                icon: "success",
+                                text: "Deleted customer",
+                            });
+                        })
+                        .catch(e => console.log(e));                    
                 }
-              })
-           
-
-            this.uri = cliente._links.self.href
-            axios.delete(this.uri)
-            .then(this.cargarDatos)
-            .catch(e => console.log(e));
+            })
+                .catch(e => console.log(e));
         },
 
-        modificarCliente(cliente){
-            this.uri = cliente._links.self.href
+        modificarCliente(cliente) {
+            this.uri = this.uri + cliente.id;
             this.nombreReactivo = cliente.firstName;
             this.apellidoReactivo = cliente.lastName;
             this.emailReactivo = cliente.email;
         },
 
-        confirmarModificacion(){
-            if(this.nombreReactivo != "" && this.apellidoReactivo != "" && this.emailReactivo !="" && this.emailReactivo.includes("@")){
+        confirmarModificacion() {
+            if (this.nombreReactivo != "" && this.apellidoReactivo != "" && this.emailReactivo != "" && this.emailReactivo.includes("@")) {
                 Swal.fire({
                     icon: "success",
                     text: "Modified client",
-                    confirmButtonColor: "#F5AE53"
                 });
 
                 axios.put(this.uri, {
@@ -106,17 +100,14 @@ createApp({
                     lastName: this.apellidoReactivo,
                     email: this.emailReactivo
                 })
-                .then(this.cargarDatos)
-                .catch(e => console.log(e));
-            }else{
+                    .then(this.cargarDatos)
+                    .catch(e => console.log(e));
+            } else {
                 Swal.fire({
                     icon: "error",
                     text: "Invalid values",
-                    confirmButtonColor: "#F5AE53"
                 });
             }
-            }
-
+        }
     },
-    
 }).mount("#app");
