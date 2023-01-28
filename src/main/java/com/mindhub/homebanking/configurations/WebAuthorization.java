42af485aa42e0c10e83e4d9ba0ebc8f88a8af7c5
JuanS <97115938+JuansARG@users.noreply.github.com>
2023-01-28 17:30:45 -0300

    corrigiendo link de login y logout

--- src/main/java/com/mindhub/homebanking/configurations/WebAuthorization.java
+++ src/main/java/com/mindhub/homebanking/configurations/WebAuthorization.java
@@ -24,7 +24,8 @@ public class WebAuthorization {
 
         http.authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/web/login.html", "/assets/**").permitAll()
-                .antMatchers(HttpMethod.POST, "/api/clients").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/clients", "/api/login").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/logout").hasAuthority("CLIENT")
                 .antMatchers(HttpMethod.GET, "/manager.html", "/h2-console", "/rest/**").hasAuthority("ADMIN")
                 .antMatchers(HttpMethod.GET,"/web/**", "/api/clients/current").hasAuthority("CLIENT")
 //                .antMatchers(HttpMethod.GET, "/api/loans").hasAuthority("CLIENT")
